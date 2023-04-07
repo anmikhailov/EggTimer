e4c62604ecad5d1a1b017bbf7520542df354a4e3
Andrey <mands315@gmail.com>
2023-04-07 22:35:40 +0300

    Add buttons actions, timer, progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,12 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var remainingProgressBar: UIProgressView!
+    
+    let eggTimes = [
+        "Soft": 10,
+        "Medium": 420,
+        "Hard": 720,
+    ]
+    
+    var secondsPassed = 0
+    var secondsTotal = 0
+    var timer = Timer()
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        if let hardness = sender.currentTitle {
+            secondsTotal = eggTimes[hardness] ?? 0
+            secondsPassed = 0
+            remainingProgressBar.progress = 0.0
+            titleLabel.text = hardness
+            timer = Timer.scheduledTimer(
+                timeInterval: 1.0,
+                target: self,
+                selector: #selector(updateCounter),
+                userInfo: nil,
+                repeats: true)
+        }
+
     }
-
-
+    
+    @objc func updateCounter() {
+        if secondsPassed < secondsTotal {
+            secondsPassed += 1
+            print("\(secondsPassed) seconds passed")
+            remainingProgressBar.progress = Float(secondsPassed) / Float(secondsTotal)
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
+    }
     
 }
 
